05f8081541cb5fb70cbd4ea9f87812cd665fdf1c
Subbu <svenkat683@gmail.com>
2025-08-29 19:37:55 +0530

    feat(nvim): add clipboard and harpoon

--- nvim/.config/nvim/lua/custom/plugins/clipboard.lua
+++ nvim/.config/nvim/lua/custom/plugins/clipboard.lua
@@ -1,10 +1,24 @@
 return {
-  "AckslD/nvim-neoclip.lua",
+  'AckslD/nvim-neoclip.lua',
   dependencies = {
-    {'kkharji/sqlite.lua', module = 'sqlite'},
-    {'nvim-telescope/telescope.nvim'},
+    { 'kkharji/sqlite.lua', module = 'sqlite' },
+    { 'nvim-telescope/telescope.nvim' },
   },
   config = function()
-    require('neoclip').setup()
+    require('neoclip').setup {
+      keys = {
+        telescope = {
+          i = {
+            paste = '<cr>',
+            paste_behind = '<c-k>',
+            replay = '<c-q>', -- replay a macro
+            delete = '<c-d>', -- delete an entry
+            edit = '<c-e>', -- edit an entry
+            custom = {},
+          },
+        },
+      },
+    }
   end,
 }
+
