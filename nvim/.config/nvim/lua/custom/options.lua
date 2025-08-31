5db8ae66df2a6b56fc4a631dfc8d994018aa7b08
Subbu <svenkat683@gmail.com>
2025-08-31 20:16:35 +0530

    feat: addition vim options
    Followed thePrimeagen config

--- nvim/.config/nvim/lua/custom/options.lua
+++ nvim/.config/nvim/lua/custom/options.lua
@@ -1,11 +1,31 @@
 -- Set tabstop, shiftwidth, and expandtab to 2 spaces
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 -- Enable smart auto-indentation based on context
 vim.opt.autoindent = true
 vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
 
 -- views can only be fully collapsed with the global statusline
 vim.opt.laststatus = 3
